7513d342ffa84df191d277b8f1be25a05e41e826
srendong <46844060+srendong@users.noreply.github.com>
2019-07-02 18:41:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    change: 0
+  }
+
+  handleChange = (e) => {
+    const count = e.target.value
+     this.setState({change: count.length})
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.handleChange}></textarea>
+        <div className="counter">{this.state.change}</div>
       </div>
     );
   }
 }
-
 export default App;
